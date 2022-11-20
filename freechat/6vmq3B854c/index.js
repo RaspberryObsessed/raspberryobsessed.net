1b7dd5c353cc249ce2100dece8a2ce8f40807c33
RaspberryObsessed <110307769+RaspberryObsessed@users.noreply.github.com>
2022-11-20 10:29:12 -0800

    Add files via upload

--- freechat/6vmq3B854c/index.js
+++ freechat/6vmq3B854c/index.js
@@ -1,17 +1,21 @@
-firebase.initializeApp({
-  // Your firebase configuration object
-});
+import firebase from "firebase/app";
+import "firebase/app-check";
 
-const appCheck = firebase.appCheck();
-// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// key is the counterpart to the secret key you set in the Firebase console.
-appCheck.activate(
-  '6LflWR4jAAAAAFA-ydDZaRRTue4a_2DyTcvcZiA1',
+function initialize() {
+  firebase.initializeApp({
+    // Your firebase configuration object
+  });
 
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
-  true);
+  const appCheck = firebase.appCheck();
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // key is the counterpart to the secret key you set in the Firebase console.
+  appCheck.activate(
+    '6LflWR4jAAAAAFA-ydDZaRRTue4a_2DyTcvcZiA1',
 
+    // Optional argument. If true, the SDK automatically refreshes App Check
+    // tokens as needed.
+    true);
+}
 
 // We enclose this in window.onload.
 // So we don't have ridiculous errors.
